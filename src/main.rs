abf60e2b115b70707b54b1b880d9b893288ee2bd
Anders-Petter Ljungquist <anderspl@axis.com>
2024-05-28 15:10:23 +0200

    Use hello_world as example
    This is the most generic example app from acap-rs.
    
    We use one of the examples from acap-rs to minimize the risk of the
    template not building after updates to the build system (will be more
    relevant once the build system is a rust binary crate that can be
    shared as opposed to an ad hoc `Makefile`).
    
    We choose the most generic example to make the template more
    approachable and a minimal starting point that users can add to rather
    than remove from.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
-//! A simple example application demonstrating how the licensekey crate may be used
+//! A simple hello world application
+//!
+//! Uses some common app-logging crates to demonstrate
+//! 1. how to use them in an application, and
+//! 2. how to build and bundle them as an application.
 
-use log::{info, warn};
-use std::ffi::{CStr, CString};
-use std::os::unix::ffi::OsStrExt;
-
-mod app_logging;
-
-const APP_ID: i32 = 0;
-const MAJOR_VERSION: i32 = 1;
-const MINOR_VERSION: i32 = 0;
-
-fn check_license_status(app_name: &CStr) {
-    match licensekey::verify(app_name, APP_ID, MAJOR_VERSION, MINOR_VERSION) {
-        Ok(()) => info!("License key is valid"),
-        Err(e) => warn!("License key is invalid because {e}"),
-    }
-}
+use log::info;
 
 fn main() {
     app_logging::init_logger();
-    let app_name = CString::new(
-        std::env::current_exe()
-            .unwrap()
-            .file_name()
-            .unwrap()
-            .as_bytes(),
-    )
-    .unwrap();
-    loop {
-        check_license_status(&app_name);
-        std::thread::sleep(std::time::Duration::from_secs(300));
-    }
+    info!("Hello World!");
 }
